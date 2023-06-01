de81c67146e6c3a0af156d7803f23811ec217cab
m6z1 <sonmyungj1zz@gmail.com>
2023-06-01 22:03:33 +0900

    Feat : 카카오맵 API 연동

--- BusTaME/app/src/main/java/com/sesac/bustame/MainActivity.kt
+++ BusTaME/app/src/main/java/com/sesac/bustame/MainActivity.kt
@@ -2,15 +2,16 @@ package com.sesac.bustame
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
 import com.sesac.bustame.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-
     private lateinit var binding: ActivityMainBinding
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
     }
 }
+
