525bf1c46d0a87228eabff9cceaa0a9040875b6d
m6z1 <sonmyungj1zz@gmail.com>
2023-06-02 21:06:04 +0900

    Feat : 위치 권한 요청 추가

--- BusTaME/app/src/main/java/com/sesac/bustame/MainActivity.kt
+++ BusTaME/app/src/main/java/com/sesac/bustame/MainActivity.kt
@@ -1,20 +1,83 @@
 package com.sesac.bustame
 
+import android.content.pm.PackageManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import com.sesac.bustame.databinding.ActivityMainBinding
 import net.daum.mf.map.api.MapView
+import android.Manifest
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var mapView: MapView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val mapView = MapView(this)
+        //권한 ID 선언
+        val InternetPermission = ContextCompat.checkSelfPermission(
+            this,
+            Manifest.permission.INTERNET
+        )
 
-        val mapViewContainer = findViewById<ViewGroup>(R.id.mapView)
-        mapViewContainer.addView(mapView)
+        val FineLocaPermission = ContextCompat.checkSelfPermission(
+            this,
+            Manifest.permission.ACCESS_FINE_LOCATION
+        )
 
+        val CoarseLocaPermission = ContextCompat.checkSelfPermission(
+            this,
+            Manifest.permission.ACCESS_COARSE_LOCATION
+        )
+
+        // 권한이 열려있는지 확인
+        if (InternetPermission == PackageManager.PERMISSION_DENIED || FineLocaPermission == PackageManager.PERMISSION_DENIED || CoarseLocaPermission == PackageManager.PERMISSION_DENIED) {
+            // 마쉬멜로우 이상버전부터 권한을 물어본다
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                // 권한 체크(READ_PHONE_STATE의 requestCode를 1000으로 세팅
+                requestPermissions(
+                    arrayOf(
+                        Manifest.permission.INTERNET,
+                        Manifest.permission.ACCESS_FINE_LOCATION,
+                        Manifest.permission.ACCESS_COARSE_LOCATION
+                    ),
+                    1000
+                )
+            }
+        }
+
+        //지도 출력
+        mapView = MapView(this)
+        binding.mapView.addView(mapView)
+
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == 1000) {
+            var checkResult = true
+
+            // 모든 퍼미션을 허용했는지 체크
+            for (result in grantResults) {
+                if (result != PackageManager.PERMISSION_GRANTED) {
+                    checkResult = false
+                    break
+                }
+            }
+
+            // 권한 체크에 동의를 하지 않으면 안드로이드 종료
+            if (!checkResult) {
+                finish()
+            }
+        }
     }
 }
