9ad5a6f4ebfa820f9f23d7b322f31a26d62ca25b
m6z1 <sonmyungj1zz@gmail.com>
2023-06-04 00:50:55 +0900

    Feat : 현재 위치 기반 주변 버스정류장 마커 표시

--- BusTaME/app/src/main/java/com/sesac/bustame/MainActivity.kt
+++ BusTaME/app/src/main/java/com/sesac/bustame/MainActivity.kt
@@ -8,11 +8,21 @@ import androidx.core.content.ContextCompat
 import com.sesac.bustame.databinding.ActivityMainBinding
 import net.daum.mf.map.api.MapView
 import android.Manifest
+import android.content.ClipData
 import android.content.Intent
+import android.util.Log
 import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.gson.Gson
 import com.google.gson.JsonObject
+import com.google.gson.reflect.TypeToken
+import net.daum.mf.map.api.MapPOIItem
 import net.daum.mf.map.api.MapPoint
 import net.daum.mf.map.api.MapView.CurrentLocationEventListener
+import okhttp3.ResponseBody
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
 import java.util.function.LongToIntFunction
 
 class MainActivity : AppCompatActivity() {
@@ -20,22 +30,19 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var mapView: MapView
     private var isInitialLocationTracked = false
+    private lateinit var locationJson: JsonObject
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        locationJson = JsonObject()
+
         //바텀시트
         BottomSheetBehavior.from(binding.bottomSheet).apply {
             peekHeight = 200
             this.state = BottomSheetBehavior.STATE_EXPANDED
-        }
-
-        //주변 정류장 확인하기 눌렀을 때
-        binding.imgIcCheckBJ.setOnClickListener {
-            val intent = Intent(this, BellActivity::class.java)
-            startActivity(intent)
         }
 
         //권한 ID 선언
@@ -78,6 +85,57 @@ class MainActivity : AppCompatActivity() {
             MapView.CurrentLocationTrackingMode.TrackingModeOnWithoutHeading
         mapView.setShowCurrentLocationMarker(true)
 
+        //주변 정류장 확인하기 눌렀을 때
+        binding.imgIcCheckBJ.setOnClickListener {
+            // 현재 위치 기준으로 정류장 확인을 위해 위치 추적 중지
+            if (!isInitialLocationTracked) {
+                isInitialLocationTracked = true
+                mapView.currentLocationTrackingMode =
+                    MapView.CurrentLocationTrackingMode.TrackingModeOff
+            }
+
+            //현재 위치 좌표 서버로 보내기
+            val call = RetrofitClient.service.sendUserPosData(locationJson)
+            call.enqueue(object : Callback<ResponseBody> {
+                override fun onResponse(
+                    call: Call<ResponseBody>,
+                    response: Response<ResponseBody>
+                ) {
+                    if (response.isSuccessful) {
+                        val responseData = response.body()?.string()
+                        val itemType = object : TypeToken<List<aroundBusStop>>() {}.type
+                        val itemList: List<aroundBusStop> = Gson().fromJson(responseData, itemType)
+
+                        // itemList를 처리하고 지도 위에 마커를 표시합니다
+                        for (item in itemList) {
+                            val latitude = item.gpsY.toDouble()
+                            val longitude = item.gpsX.toDouble()
+
+                            // 지도 위에 마커 표시
+                            val marker = MapPOIItem()
+                            marker.itemName = item.stationNm
+                            marker.mapPoint = MapPoint.mapPointWithGeoCoord(latitude, longitude)
+                            // 원하는 대로 마커의 모양을 커스터마이징할 수 있습니다
+                            // ...
+
+                            // 마커를 지도에 추가합니다
+                            mapView.addPOIItem(marker)
+
+                        }
+                    } else {
+                        // 서버로부터 실패 응답을 받은 경우 처리
+                        Log.d("Fail Response", "FailFailFailFailFailFailFailFail")
+                        Log.d("responsecode", response.code().toString())
+                    }
+                }
+
+                override fun onFailure(call: Call<ResponseBody>, t: Throwable) {
+                    // 통신 실패 처리
+                    Log.d("Failure", "FailFailFailFailFailFailFailFail")
+                }
+            })
+        }
+
 
         //현재 위치 이벤트 리스너
         mapView.setCurrentLocationEventListener(object : CurrentLocationEventListener {
@@ -89,21 +147,17 @@ class MainActivity : AppCompatActivity() {
             ) {
                 val latitude = mapPoint.mapPointGeoCoord.latitude //위도
                 val longitude = mapPoint.mapPointGeoCoord.longitude //경도
+                val radius = 300
 
                 val locationJson = JsonObject()
+
                 locationJson.apply {
                     addProperty("tmX", latitude)
                     addProperty("tmY", longitude)
+                    addProperty("radius", radius)
 
-                    // 처음 위치 추적 후 추적 중지
-                    if (!isInitialLocationTracked) {
-                        // 처음 위치 추적 후 추적 중지
-                        isInitialLocationTracked = true
-                        mapView.currentLocationTrackingMode =
-                            MapView.CurrentLocationTrackingMode.TrackingModeOff
-
-                    }
                 }
+
             }
 
             override fun onCurrentLocationDeviceHeadingUpdate(mapView: MapView, v: Float) {
---
+++ BusTaME/app/src/main/java/com/sesac/bustame/aroundBusStop.kt
@@ -0,0 +1,13 @@
+package com.sesac.bustame
+
+data class aroundBusStop(
+    val posX: String,
+    val stationTp: String,
+    val arsId: String,
+    val posY: String,
+    val stationNm: String,
+    val dist: String,
+    val gpsX: String,
+    val gpsY: String,
+    val stationId: String
+)
