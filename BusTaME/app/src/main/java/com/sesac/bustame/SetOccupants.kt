1e80e0ccb2aebbad4c4b106a6a1a8dbb4752f71d
m6z1 <sonmyungj1zz@gmail.com>
2023-06-03 22:54:53 +0900

    Feat : 탑승자 유형 화면

---
+++ BusTaME/app/src/main/java/com/sesac/bustame/SetOccupants.kt
@@ -0,0 +1,11 @@
+package com.sesac.bustame
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SetOccupants : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_set_occupants)
+    }
+}
